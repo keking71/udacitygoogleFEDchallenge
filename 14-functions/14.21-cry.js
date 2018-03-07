25779a01d8d25688d79b6f19fbeca416f3463005
Katie King <kesaxton1@gmail.com>
2018-03-06 19:21:53 -0500

    Lesson 14 named function expression quiz

---
+++ 14-functions/14.21-cry.js
@@ -0,0 +1,9 @@
+/*
+ * Programming Quiz: Cry (5-5)
+ */
+
+ var cry = function sob(){ //run named function
+   return "boohoo!"; //return value
+ };
+
+ console.log(cry()); //print var to the console
